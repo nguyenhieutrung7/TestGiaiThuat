c8a6480dac2693f45dcac397e48e0c508086097c
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2018-10-15 17:37:55 +0700

    linhtinh

--- PhanTichVaThietKeGiaiThuat/Program.cs
+++ PhanTichVaThietKeGiaiThuat/Program.cs
@@ -11,14 +11,30 @@ namespace PhanTichVaThietKeGiaiThuat
         public static float ExP(int x, int n)
         {
             float t = 1;
-            float gt = 1;
+            float temp = 1;
             for (int i = 1; i <= n; i++)
             {
-                gt = gt * i;
-                float temp = x / gt;
+                temp = temp *x/ i;
                 t = t + temp;
             }
             return t;
+        }
+        public static int GiaiThua(int n)
+        {
+            int gt = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                gt *= i;
+            }
+            return gt;
+        }
+        public static int DeQuiGiaiThua(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else return n * DeQuiGiaiThua(n - 1);
         }
         static void Main(string[] args)
         {
@@ -26,6 +42,8 @@ namespace PhanTichVaThietKeGiaiThuat
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             float exp = ExP(4,5);
             Console.WriteLine(exp);
+            Console.WriteLine("Giai thua:" + GiaiThua(5));
+            Console.WriteLine("Giai thua de qui:"+DeQuiGiaiThua(1));
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
