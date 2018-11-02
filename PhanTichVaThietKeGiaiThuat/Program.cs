2fca8ac44e4f32e28f3886ad9f305f92d87e680c
nguyenhieutrung7 <44111310+nguyenhieutrung7@users.noreply.github.com>
2018-11-02 17:18:49 +0700

    Commit
    Commit cafe cheese

--- PhanTichVaThietKeGiaiThuat/Program.cs
+++ PhanTichVaThietKeGiaiThuat/Program.cs
@@ -9,11 +9,19 @@ namespace PhanTichVaThietKeGiaiThuat
 {
     class Program
     {
-        public static void countArray(int[] a, int n)
+        public static void coutArray(int[] a)
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < a.Count(); i++)
             {
                 Console.Write("a[" + i + "]:" + a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void coutArray2(int[] a)
+        {
+            for (int i = 0; i < a.Count(); i++)
+            {
+                Console.Write(a[i] + " ");
             }
             Console.WriteLine();
         }
@@ -94,23 +102,48 @@ namespace PhanTichVaThietKeGiaiThuat
             }
             return min;
         }
+        public static int[] C3_2017_2018(int[] a,int[] b)
+        {
+            int[] c = new int[a.Count()];
+            int count = 0;
+            for(int i=0;i<a.Count();i++)
+                for(int j = 0; j < b.Count(); j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        c[count] = a[i];
+                        count++;
+                        break;
+                    }
+                }
+            return c;
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            float exp = ExP(4,5);
-            Console.WriteLine(exp);
-            Console.WriteLine("Giai thua:" + Factorial(5));
-            Console.WriteLine("Giai thua de qui:"+ RecursionFactorial(1));
+            //float exp = ExP(4,5);
+            //Console.WriteLine(exp);
+            //Console.WriteLine("Giai thua:" + Factorial(5));
+            //Console.WriteLine("Giai thua de qui:"+ RecursionFactorial(1));
             int[] a = CreateRamdomArrayInteger(15);
             Console.Write("Mảng ngẫu nhiên được tạo: ");
-            countArray(a,a.Count());
-            Point point = new Point();
-            point = ClosestPair(a,a.Count());
-            Console.WriteLine("Vị trí cặp điểm gần nhau nhất: "+point.x1+","+point.x2);
-            int cp = ClosestPairUpdate(a,a.Count());
-            Console.WriteLine("Khoảng cách đó là: " + cp);
+            coutArray(a);
+            //Point point = new Point();
+            //point = ClosestPair(a,a.Count());
+            //Console.WriteLine("Vị trí cặp điểm gần nhau nhất: "+point.x1+","+point.x2);
+            //int cp = ClosestPairUpdate(a,a.Count());
+            //Console.WriteLine("Khoảng cách đó là: " + cp);
+            int[] b = CreateRamdomArrayInteger(15);
+            Console.Write("Mảng ngẫu nhiên được tạo: ");
+            coutArray(b);
+            coutArray2(a);
+            coutArray2(b);
+            Console.WriteLine("Mảng giao: ");
+            int[] c = C3_2017_2018(a, b);
+            coutArray(c);
+            
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
